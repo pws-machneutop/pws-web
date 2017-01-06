5607679322223c290d35ef731d65183ecba4f11f
siryusuk <robinwacco11@hotmail.com>
2017-01-06 10:13:07 +0000

    add class mtop to style.css

--- js/josh.js
+++ js/josh.js
@@ -5,7 +5,7 @@ var josh = {
 
         var command = document.createElement("div");
         command.className = "dostext";
-        command.innerHTML = '<table width="100%" border="0"><tr><td width="30">J:\\&gt; ' + iv_input.value + '</td></tr></table>';
+        command.innerHTML = '<table width="100%" border="0" class="mtop"><tr><td width="30">J:\\&gt; ' + iv_input.value + '</td></tr></table>';
         invoervak.parentNode.insertBefore(command, invoervak);
 
         iv_input.value = "";
